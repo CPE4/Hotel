bb0cd2a319d3e59f7adc2179d0ff489972f9d300
DamienCPE <dami184@live.fr>
2014-10-20 16:43:52 +0200

    changement

--- ClientHotel/Program.cs
+++ ClientHotel/Program.cs
@@ -16,7 +16,7 @@ namespace ClientHotel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Hotels());
         }
     }
 }
